79b32ca36c7d573f7994711b70df24a703867d90
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-05-24 20:15:19 +0430

    First test and logic
    Complete first test of app and add a few logic to Calc.ts (getOperations method created)

--- src/modules/Calc.ts
+++ src/modules/Calc.ts
@@ -4,8 +4,32 @@ export enum InputType {
     Number, Operation
 }
 
-const getState = () => {
+export enum operatorType {Add , Sub , Equal}
+export type Operation = {
+    operator: operatorType,
+    value: number
+}
 
+export type OperationBuilder = {
+    operations: Operation[],
+    working: Omit<Operation , 'operation'> | null;
+}
+
+export type CalcInput = {type: InputType.Number , value: number} | {type: InputType.Operation , operator: operatorType}
+
+export type CalcOutput = {displayValue: number}
+
+const getOperation = (inputs: CalcInput[]): Operation[] => {
+    const builder = inputs.reduce((build , input): OperationBuilder[] => {
+        switch (input.type) {
+            case InputType.Number:
+                const previous = build.working?.value;
+        }
+    } , {operations: [] , working: null} as OperationBuilder)
+}
+
+const getState = (inputs: Array<CalcInput>):CalcOutput => {
+    return {displayValue: 0}
 }
 
 const Calc = {
--- src/modules/Cals.test.ts
+++ src/modules/Cals.test.ts
@@ -1,5 +1,18 @@
 import '@testing-library/react'
+import {CalcInput, InputType , operatorType } from './Calc'
+import Calc from './Calc'
 
-test('' , () => {
-    
+
+test('derive state' , () => {
+    const inputs: CalcInput[] = [
+        {type: InputType.Number , value: 1},
+        {type: InputType.Number , value: 2},
+        {type: InputType.Operation , operator: operatorType.Add},
+        {type: InputType.Number , value: 3},
+        {type: InputType.Operation , operator: operatorType.Equal}
+    ]
+
+    const result = Calc.getState(inputs)
+
+    expect(result.displayValue).toEqual(15);
 })
