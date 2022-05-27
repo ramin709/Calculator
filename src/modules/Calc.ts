35470f2f3997aeaadc5d158804162a7c35a64b74
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-05-27 16:19:34 +0430

    Test and Logic
    Add another test to check getOperation method and a few complete that function

--- src/modules/Calc.ts
+++ src/modules/Calc.ts
@@ -4,7 +4,7 @@ export enum InputType {
     Number, Operation
 }
 
-export enum operatorType {Add , Sub , Equal}
+export enum operatorType { Add = 'add', Sub = 'sub', Equal = 'equal', }
 export type Operation = {
     operator: operatorType,
     value: number
@@ -12,28 +12,41 @@ export type Operation = {
 
 export type OperationBuilder = {
     operations: Operation[],
-    working: Omit<Operation , 'operation'> | null;
+    working: Operation;
 }
 
-export type CalcInput = {type: InputType.Number , value: number} | {type: InputType.Operation , operator: operatorType}
+export type CalcInput = { type: InputType.Number, value: number } | { type: InputType.Operation, operator: operatorType }
 
-export type CalcOutput = {displayValue: number}
+export type CalcOutput = { displayValue: number }
 
 const getOperation = (inputs: CalcInput[]): Operation[] => {
-    const builder = inputs.reduce((build , input): OperationBuilder[] => {
+    const builder: OperationBuilder = inputs.reduce<OperationBuilder>((builder, input): OperationBuilder => {
         switch (input.type) {
             case InputType.Number:
-                const previous = build.working?.value;
+                const previous = builder.working?.value || 0;
+                const newValue = previous * 10 + input.value;
+                return { ...builder, working: { ...builder.working, value: newValue } }
+
+            case InputType.Operation:
+                if (input.operator === operatorType.Equal) {
+                    return { operations: [...builder.operations, builder.working, { operator: operatorType.Equal, value: 0 }], working: { operator: operatorType.Add, value: 0 } }
+
+                } else {
+                    return { operations: [...builder.operations, builder.working], working: { operator: input.operator, value: 0 } }
+                }
         }
-    } , {operations: [] , working: null} as OperationBuilder)
+    }, { operations: [], working: { operator: operatorType.Add, value: 0 } })
+
+    return builder.operations;
 }
 
-const getState = (inputs: Array<CalcInput>):CalcOutput => {
-    return {displayValue: 0}
+const getState = (inputs: Array<CalcInput>): CalcOutput => {
+    return { displayValue: 0 }
 }
 
 const Calc = {
-    getState
+    getState,
+    getOperation
 }
 
 export default Calc
--- src/modules/Cals.test.ts
+++ src/modules/Cals.test.ts
@@ -1,18 +1,36 @@
 import '@testing-library/react'
-import {CalcInput, InputType , operatorType } from './Calc'
+import { CalcInput, InputType, Operation, operatorType } from './Calc'
 import Calc from './Calc'
 
-
-test('derive state' , () => {
+test('generate operations', () => {
     const inputs: CalcInput[] = [
-        {type: InputType.Number , value: 1},
-        {type: InputType.Number , value: 2},
-        {type: InputType.Operation , operator: operatorType.Add},
-        {type: InputType.Number , value: 3},
-        {type: InputType.Operation , operator: operatorType.Equal}
+        { type: InputType.Number, value: 1 },
+        { type: InputType.Number, value: 2 },
+        { type: InputType.Operation, operator: operatorType.Add },
+        { type: InputType.Number, value: 3 },
+        { type: InputType.Operation, operator: operatorType.Equal }
+    ]
+
+    const operations: Operation[] = [
+        {operator: operatorType.Add, value: 12},
+        {operator: operatorType.Add, value: 3},
+        {operator: operatorType.Equal, value: 0},
+    ]
+
+    expect(Calc.getOperation(inputs)).toEqual(operations);
+})
+
+
+/* test('derive state', () => {
+    const inputs: CalcInput[] = [
+        { type: InputType.Number, value: 1 },
+        { type: InputType.Number, value: 2 },
+        { type: InputType.Operation, operator: operatorType.Add },
+        { type: InputType.Number, value: 3 },
+        { type: InputType.Operation, operator: operatorType.Equal }
     ]
 
     const result = Calc.getState(inputs)
 
     expect(result.displayValue).toEqual(15);
-})
+}) */
